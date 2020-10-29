35140d54c1446d0d34c75ec2f044e0ac0c485a29
minjung-s <smj139052@naver.com>
2020-10-29 11:40:59 +0900

    str

--- 1.문자열/validPalindrome.py
+++ 1.문자열/validPalindrome.py
@@ -2,11 +2,19 @@ import re
 def validPalindrome(s):
     sentence = []
     for i in s:
-        if 64<ord(i)<91 or 96<ord(i)<123 :
+        if 64<ord(i)<91 :
             sentence.append(i)
+        elif 96<ord(i)<123 :
+            sentence.append(chr(ord(i)-32))
 
-    
-    return sentence
+    condition = True
+    listlen = len(sentence)//2
+    for i in range(listlen-1):
+        if sentence[i] != sentence[len(sentence)-i-1] :
+            condition = False
+            break
+
+    return condition
 
 def main():
     s = input('문자열을 입력하세요: ')
