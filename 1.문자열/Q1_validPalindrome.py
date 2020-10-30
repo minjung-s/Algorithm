2af20e2bde1739828ca6e2b096419aec89531dc1
minjung-s <smj139052@naver.com>
2020-10-30 15:17:49 +0900

    str

--- 1.문자열/Q1_validPalindrome.py
+++ 1.문자열/Q1_validPalindrome.py
@@ -1,26 +1,18 @@
 import re
-def validPalindrome(s):
-    sentence = []
-    for i in s:
-        if 64<ord(i)<91 :
-            sentence.append(i)
-        elif 96<ord(i)<123 :
-            sentence.append(chr(ord(i)-32))
+class Solution:     
+    def isPalindrome(self, s: str) -> bool:
+        sentence = []
+        for i in s:
+            if 64<ord(i)<91 :
+                sentence.append(i)
+            elif 96<ord(i)<123 :
+                sentence.append(chr(ord(i)-32))
 
-    condition = True
-    listlen = len(sentence)//2
-    for i in range(listlen-1):
-        if sentence[i] != sentence[len(sentence)-i-1] :
-            condition = False
-            break
-        
-    return condition
+        condition = True
+        listlen = len(sentence)//2
+        for i in range(listlen-1):
+            if sentence[i] != sentence[len(sentence)-i-1] :
+                condition = False
+                break
 
-
-def main():
-    s = input('문자열을 입력하세요: ')
-    print(validPalindrome(s))
-
-
-if __name__ == '__main__':
-    main()
+        return condition
--- 1.문자열/Q2_reverseString.py
+++ 1.문자열/Q2_reverseString.py
@@ -1,12 +1,6 @@
-def reverseString(s):
-    str2list = list(s)
-    list2str = ''.join(list(reversed(str2list)))
+class Solution:
+    def reverseString(self, s: str) -> None:
+        str2list = list(s)
+        list2str = ''.join(list(reversed(str2list)))
+        return list2str
 
-    return list2str
-
-def main():
-    s = input('문자열을 입력하세요: ')
-    print(reverseString(s))
-
-if __name__ == '__main__':
-    main()
