78b61c09b3fc73f8702a9fa0cb7c33396d20981f
minjung-s <smj139052@naver.com>
2020-11-05 13:30:58 +0900

    str HW

--- 1.문자열/Q8_HW2.py
+++ 1.문자열/Q8_HW2.py
@@ -1,23 +1,22 @@
 import re 
-#while True: 
- 
-string = "ZGBG "
-
-
-list_str = list(string)
-check = 1
-for i,element in enumerate(range(0,len(list_str)-2)) :
-    list_tmp = list_str[i+1:]
-    result = []
-    for j in range(len(list_tmp)):
-        result.append(list_str[j] +list_tmp[j])
-    result_set = set(result)
-    print(list(result_set))
-    print(result)
+while True: 
     
-    if list(result_set).sort() != result.sort():
-        check = 0
-if check == 0:
-    print(string +" is not surprising.")
-else : 
-    print(string +" is surprising.")
+    string = "ZGBG "
+    if string == "*":
+        break
+
+    list_str = list(string)
+    check = 1
+    for i,element in enumerate(range(0,len(list_str)-2)) :
+        list_tmp = list_str[i+1:]
+        result = []
+        for j in range(len(list_tmp)):
+            result.append(list_str[j] +list_tmp[j])
+        result_set = set(result)
+        
+        if list(result_set).sort() != result.sort():
+            check = 0
+    if check == 0:
+        print(string +" is not surprising.")
+    else : 
+        print(string +" is surprising.")
